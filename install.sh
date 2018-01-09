8f0fd0188deb96afc5e5b905bfd524eb5b2bdba8
James Devine <devine.jd@gmail.com>
2018-01-09 18:17:04 +0100

    was copying the wrong dhcpcd

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ cp -f hostapd /etc/default/hostapd
 cp -f hostapd.conf /etc/hostapd/
 mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 cp -f dnsmasq.conf /etc/
-cp -f dhcpcdstatic.conf /etc/dhcpcd.conf
+cp -f dhcpcd.conf /etc/dhcpcd.conf
 #cp -f rc.local /etc/
 sudo systemctl enable hostapd   
 sudo systemctl enable dnsmasq   
